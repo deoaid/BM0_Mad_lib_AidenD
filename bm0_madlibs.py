f457e5cfe4e0164b3db2028f205b617e15eb2e44
deoaid <aidend46@nycstudents.net>
2025-12-01 14:52:23 -0500

    Update bm0_madlibs.py

--- bm0_madlibs.py
+++ bm0_madlibs.py
@@ -1,2 +1,11 @@
-print("Hello World")
+person = input("Enter a person:")
+place = input("Enter a place:")
+thing = input("Enter a thing:")
+import random
 
+
+
+if random.randint(1,2) == 2:
+		print(f"{person} is walking in the airport as they just landed in {place} but they forgot their {thing}.")
+else:
+	print(f"Many people hate on {person} due to their love of {thing}, people from their hometown {place} still support them.")
